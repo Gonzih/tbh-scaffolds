27641989e5d5909080344bf193a12f401c699458
Max Gonzih <gonzih@gmail.com>
2011-09-15 20:15:26 +0300

    set rails template engine to haml, fix error spans

--- lib/tbh-scaffolds.rb
+++ lib/tbh-scaffolds.rb
@@ -2,6 +2,22 @@ require "tbh-scaffolds/version"
 
 module Tbh
   module Scaffolds
-    # Your code goes here...
+    class Engine < ::Rails::Engine
+    end
+  end
+
+  module Rails
+    class Railtie < ::Rails::Railtie
+      if ::Rails.version.to_f >= 3.1
+        config.app_generators.template_engine :haml
+      else
+        config.generators.template_engine :haml
+      end
+
+      config.before_initialize do
+        Haml.init_rails(binding)
+        Haml::Template.options[:format] = :html5
+      end
+    end
   end
 end
